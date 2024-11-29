d8611e2b2319f0ea2039a2a6ee9eed9830a11540
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-11-29 16:10:00 +0530

    updated backend files

---
+++ Backend/lms_backend/src/main/java/com/lms/lms_backend/LmsBackendApplication.java
@@ -0,0 +1,10 @@
+package com.lms.lms_backend;
+
+
+public class LmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/lms_backend/src/test/java/com/lms/lms_backend/LmsBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lms.lms_backend;
+
+import org.junit.jupiter.api.Test;
+
+
+class LmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
